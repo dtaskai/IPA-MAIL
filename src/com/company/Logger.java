dc68be149d3161478b015287bf9a9c40d1479154
Táskai Dominik <taskaidomi@gmail.com>
2020-04-27 20:22:41 +0200

    Logger class fixed

---
+++ src/com/company/Logger.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020.
+ *
+ * Készült Táskai Dominik - AM6JEK által az IP alapú rendszerek és alkalmazások (IVB369MNMI) tantárgy keretein belül.
+ */
+
+package com.company;
+
+import javax.swing.*;
+
+public class Logger {
+    private JFrame jFrame;
+    private JPanel jPanel;
+    private JTextArea jTextAreaLog;
+
+    public Logger(){
+        setElements();
+    }
+
+    private void setElements(){
+        jFrame = new JFrame();
+        jPanel = new JPanel();
+        jTextAreaLog = new JTextArea();
+        jFrame.setSize(320,250);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setTitle("Mail Log");
+        jFrame.add(jPanel);
+        jFrame.setResizable(false);
+        jFrame.setLocationRelativeTo(null);
+        jPanel.setLayout(null);
+        jTextAreaLog.setSize(320,250);
+        jFrame.add(jTextAreaLog);
+    }
+
+    public void showLog(boolean var){
+        if(jFrame.isVisible()==true){
+            jFrame.setVisible(false);
+        }else{
+            jFrame.setVisible(true);
+        }
+    }
+
+    public void addText(String text){
+        jTextAreaLog.append(text);
+    }
+
+    public void clearLog(){
+        jTextAreaLog.setText("");
+    }
+}
